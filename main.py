7e94def8c3187e26008281ee9b06353013a8646b
WANGYUE <401360492@qq.com>
2022-11-20 23:12:15 +0800

    去除main中的多于代码

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == "__main__":
     # pytest.main(["--alluredir=./temp/my_allure_results"])   #程序入口
     pytest.main()
-    os.system("allure serve ./temp/my_allure_results")
+
 
 
 
